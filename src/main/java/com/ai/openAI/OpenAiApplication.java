acbfe55675e95b1b7f5849a628fa399b96e57a03
Rohan Jha <rohanjhaaa@gmail.com>
2026-06-06 20:17:42 +0530

    Open AI(Not configured due to API Key) & Ollama Model

---
+++ src/main/java/com/ai/openAI/OpenAiApplication.java
@@ -0,0 +1,14 @@
+package com.ai.openAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiApplication.class, args);
+		System.out.println("====================::- SPRING AI : OPEN AI Started.... -::====================");
+	}
+
+}
